197b2bf387454f52e2adf3dd86a13e01925a3f4e
Dominiqueprov <dominiquepro@hotmail.com>
2020-02-05 10:04:41 -0500

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,11 +1,61 @@
 
+
+// // switch (key) {
+// //     case value:
         
+// //         break;
+
+// //     default:
+// //         break;
+// // }
 
 
-    let theNumber = Number(prompt("Pick a number"));
-        if (!Number.isNaN(theNumber)){
-            console.log("Your number is" + theNumber * theNumber);
-        } else {
-            console.log("Hey. Why didnt you ? ");
-        }
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+    
+// // }
+
+
+// // array.forEach(element => {
+    
+// // });
+
+
+// // for (const iterator of object) {
+    
+// // }
+
+
+// let fruits = ['Apple', 'Oranges', 'Banana'];
+
+// fruits.unshift('Pineapple', 'Cherry');
+
+// console.log(fruits);
+
+
+// let fruits =  ['Apple', 'Oranges', 'Banana'];
+
+// let myFavorite = fruits.shift();
+
+// console.log(myFavorite);
+
+// console.log(fruits);
+
+// function sum (a, b, c) {
+//      return a + b + c;
+// }
+
+// console.log(sum(2,3,4));
+
+// function personName(first,last){
+//     return `${first} ${last}`
+// }
+
+// console.log(personName('Eric', 'Sanchez'));
+
+
+const personName = (first,last) => `${first} ${last}`
+
+console.log(personName('Eric', 'Sanchez'));
+
 
